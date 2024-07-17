dc4ccc4cbe76e0e26d349d52736f231b8d62ca76
Antonis Geralis <b3lievem@yandex.com>
2024-07-17 18:32:33 +0300

    add len proc

--- varchars.nim
+++ varchars.nim
@@ -6,6 +6,16 @@ type
 template `@^`(x: VarChar): untyped =
   array[N, byte](x)
 
+proc lenVarChars[N](x: VarChar[N]): tuple[len, varintLen: int] =
+  var varint: uint64
+  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
+  assert N - varintLen >= int(varint)
+  result = (int(varint), varintLen)
+
+proc len*[N](x: VarChar[N]): int {.inline.} =
+  let (result, varintLen) = x.len
+  return result
+
 proc toVarChar*[N](data: string): VarChar[N] =
   let varintLen = writeVu64(toOpenArray(@^result, 0, maxVarIntLen - 1),
       uint64(data.len))
@@ -15,10 +25,9 @@ proc toVarChar*[N](data: string): VarChar[N] =
   copyMem(addr @^result[varintLen], cstring(data), min(N - varintLen, data.len))
 
 proc toString*[N](x: VarChar[N]): string =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  assert N - varintLen >= int(varint)
-  result = newString(min(int(varint), N - varintLen))
+  let (len, varintLen) = lenVarChars(x)
+  assert N - varintLen >= len
+  result = newString(min(len, N - varintLen))
   # for i in 0 ..< result.len:
   #   result[i] = char(@^x[i + varintLen])
   copyMem(cstring(result), addr @^x[varintLen], result.len)
@@ -27,10 +36,8 @@ proc `$`*[N](x: VarChar[N]): string {.inline.} = toString(x)
 
 # Comparisons
 proc eqVarChars[N](a, b: VarChar[N]): bool =
-  var aLen: uint64
-  let aVarintLen = readVu64(toOpenArray(@^a, 0, maxVarIntLen - 1), aLen)
-  var bLen: uint64
-  let bVarintLen = readVu64(toOpenArray(@^b, 0, maxVarIntLen - 1), bLen)
+  let (aLen, aVarintLen) = lenVarChars(a)
+  let (bLen, bVarintLen) = lenVarChars(b)
   if aLen == bLen:
     if aLen == 0: return true
     return equalMem(addr @^a[aVarintLen], addr @^b[bVarintLen],
@@ -39,10 +46,8 @@ proc eqVarChars[N](a, b: VarChar[N]): bool =
 proc `==`*[N](a, b: VarChar[N]): bool {.inline.} = @^a == @^b # eqVarChars(a, b)
 
 proc cmpVarChars[N](a, b: VarChar[N]): int =
-  var aLen: uint64
-  let aVarintLen = readVu64(toOpenArray(@^a, 0, maxVarIntLen - 1), aLen)
-  var bLen: uint64
-  let bVarintLen = readVu64(toOpenArray(@^b, 0, maxVarIntLen - 1), bLen)
+  let (aLen, aVarintLen) = lenVarChars(a)
+  let (bLen, bVarintLen) = lenVarChars(b)
   let minLen = min(min(int(aLen), N - aVarintLen), min(int(bLen), N - bVarintLen))
   result = cmpMem(addr @^a[aVarintLen], addr @^b[bVarintLen], minLen)
   if result == 0:
