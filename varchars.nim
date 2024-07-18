e9ac1a3a8e054bdc0420d8ae86adec9788c14488
Antonis Geralis <b3lievem@yandex.com>
2024-07-18 05:53:01 +0300

    reduce typing

--- varchars.nim
+++ varchars.nim
@@ -6,6 +6,11 @@ type
 template `@^`(x: Varchar): untyped =
   array[N, byte](x)
 
+template readVarcharLen(x, varint, varintLen, len: untyped): untyped =
+  var varint: uint64
+  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
+  let len = min(int(varint), N - varintLen)
+
 proc toVarchar*[N](data: string): Varchar[N] =
   static: assert N >= maxVarIntLen
   let varintLen = writeVu64(toOpenArray(@^result, 0, maxVarIntLen - 1),
@@ -17,12 +22,11 @@ proc toVarchar*[N](data: string): Varchar[N] =
 
 proc toString*[N](x: Varchar[N]): string =
   static: assert N >= maxVarIntLen
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
+  readVarcharLen(x, varint, varintLen, len)
   assert N - varintLen >= int(varint)
-  result = newStringUninit(min(int(varint), N - varintLen))
+  result = newStringUninit(len)
   # for i in 0 ..< result.len:
-  #   result[i] = char(@^x[i + varintLen])
+  #   result[i] = char(@^x[i + l])
   copyMem(cstring(result), addr @^x[varintLen], result.len)
 
 proc `$`*[N](x: Varchar[N]): string {.inline.} = toString(x)
@@ -31,22 +35,19 @@ proc `$`*[N](x: Varchar[N]): string {.inline.} = toString(x)
 proc `==`*[N](a, b: Varchar[N]): bool {.inline.} = @^a == @^b
 
 proc cmpVarchars*[N](a, b: Varchar[N]): int =
-  var aLen: uint64
-  let aVarintLen = readVu64(toOpenArray(@^a, 0, maxVarIntLen - 1), aLen)
-  var bLen: uint64
-  let bVarintLen = readVu64(toOpenArray(@^b, 0, maxVarIntLen - 1), bLen)
-  let minLen = min(min(int(aLen), N - aVarintLen), min(int(bLen), N - bVarintLen))
+  readVarcharLen(a, aVarint, aVarintLen, aLen)
+  readVarcharLen(b, bVarint, bVarintLen, bLen)
+  let minLen = min(aLen, bLen)
   result = cmpMem(addr @^a[aVarintLen], addr @^b[bVarintLen], minLen)
   if result == 0:
-    result = int(aLen) - int(bLen)
+    result = int(aVarint) - int(bVarint)
 
 proc `<=`*[N](a, b: Varchar[N]): bool {.inline.} = cmpVarchars(a, b) <= 0
 proc `<`*[N](a, b: Varchar[N]): bool {.inline.} = cmpVarchars(a, b) < 0
 
 proc len*[N](x: Varchar[N]): int =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  result = min(int(varint), N - varintLen)
+  readVarcharLen(x, varint, varintLen, len)
+  result = len
 
 proc raiseIndexDefect(i, n: int) {.noinline, noreturn.} =
   raise newException(IndexDefect, "index " & $i & " not in 0 .. " & $n)
@@ -58,49 +59,39 @@ template checkBounds(i, n) =
         raiseIndexDefect(i, n-1)
 
 proc `[]`*[N](x: Varchar[N]; i: int): char {.inline.} =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  checkBounds(i, min(int(varint), N - varintLen))
+  readVarcharLen(x, varint, varintLen, len)
+  checkBounds(i, len)
   char(@^x[varintLen + i])
 
 proc `[]=`*[N](x: var Varchar[N]; i: int; val: char) {.inline.} =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  checkBounds(i, min(int(varint), N - varintLen))
+  readVarcharLen(x, varint, varintLen, len)
+  checkBounds(i, len)
   @^x[varintLen + i] = byte(val)
 
 proc `[]`*[N](x: Varchar[N]; i: BackwardsIndex): char {.inline.} =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  let len = min(int(varint), N - varintLen)
+  readVarcharLen(x, varint, varintLen, len)
   checkBounds(len - i.int, len)
   char(@^x[varintLen + len - i.int])
 
 proc `[]=`*[N](x: var Varchar[N]; i: BackwardsIndex; val: char) {.inline.} =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^x, 0, maxVarIntLen - 1), varint)
-  let len = min(int(varint), N - varintLen)
+  readVarcharLen(x, varint, varintLen, len)
   checkBounds(len - i.int, len)
   @^x[varintLen + len - i.int] = byte(val)
 
 iterator items*[N](a: Varchar[N]): char {.inline.} =
   var i = 0
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^a, 0, maxVarIntLen - 1), varint)
-  let L = min(int(varint), N - varintLen)
+  readVarcharLen(a, varint, varintLen, L)
   while i < L:
     yield char(@^a[varintLen + i])
     inc(i)
 
 template toOpenArray*[N](s: Varchar[N]; first, last: int): untyped =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^s, 0, maxVarIntLen - 1), varint)
+  readVarcharLen(s, varint, varintLen, L)
   toOpenArray(cast[cstring](addr @^s[varintLen]), first, last)
 
 template toOpenArray*[N](s: Varchar[N]): untyped =
-  var varint: uint64
-  let varintLen = readVu64(toOpenArray(@^s, 0, maxVarIntLen - 1), varint)
-  toOpenArray(cast[cstring](addr @^s[varintLen]), 0, min(int(varint), N - varintLen)-1)
+  readVarcharLen(s, varint, varintLen, L)
+  toOpenArray(cast[cstring](addr @^s[varintLen]), 0, L-1)
 
 proc hash*[N](x: Varchar[N]): Hash =
   hash(toOpenArray(x))
